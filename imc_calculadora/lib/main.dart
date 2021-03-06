e67537bb5bed0b01ef6f118f1f9bef9c4a9c7f52
Francisco Aurizelio <francisco.souza63@etec.sp.gov.br>
2021-03-06 00:39:30 -0300

    calculadora imc

---
+++ imc_calculadora/lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+  GlobalKey<FormState> _chave = GlobalKey<FormState>();
+
+  String _info = "Informe seus dados!";
+
+  void _reset() {
+    pesoController.text = "";
+    alturaController.text = "";
+    setState(() {
+      _info = "Informe seus dados!";
+    });
+  }
+
+  void _calcular() {
+    double peso = double.parse(pesoController.text);
+    double altura = double.parse(alturaController.text) / 100;
+    double imc = peso / (altura * altura);
+    setState(() {
+      if (imc < 18.5) {
+        _info = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        _info = "Peso normal(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 25.0 && imc <= 29.9) {
+        _info = "Sobrepeso(${imc.toStringAsPrecision(3)})";
+      } else if (imc >= 30.0 && imc <= 34.9) {
+        _info = "Obesidade grau 1 (${imc.toStringAsPrecision(3)})";
+      } else {
+        _info = "Obesidade grau 2 (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: _reset,
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Form(
+              key: _chave,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Icon(
+                    Icons.person_outline,
+                    size: 100.0,
+                    color: Colors.green,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Peso (Kg)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: pesoController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira seu peso!";
+                      }
+                    },
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        labelText: "Altura (cm)",
+                        labelStyle: TextStyle(color: Colors.green)),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25.0),
+                    controller: alturaController,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Insira seu altura!";
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: Container(
+                      // margin: const EdgeInsets.only(top: 10.0,bottom: 10.0),
+                      height: 70.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          if (_chave.currentState.validate()) {
+                            _calcular;
+                          }
+                        },
+                        child: Text("Calcular",
+                            style: TextStyle(
+                                color: Colors.white, fontSize: 25.0)),
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    _info,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.green, fontSize: 25),
+                  )
+                ],
+              ),
+            )
+        )
+    );
+  }
+}
